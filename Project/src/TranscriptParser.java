73d90cf30b0d99258030c2f6d39c69938db89f49
OlsonThao <thaooo@augsburg.edu>
2018-11-27 23:10:12 -0600

    removed unused libraries

--- Project/src/TranscriptParser.java
+++ Project/src/TranscriptParser.java
@@ -1,30 +1,15 @@
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import java.io.*;
 import java.util.*;
-import java.lang.*;
 
 /**
  * @author Erik, Olson, and Mahamed
